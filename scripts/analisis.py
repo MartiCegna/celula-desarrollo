301b866390f2cae010c96c2eac261d347e48fa05
MartiCegna <marticegna15@gmail.com>
2026-06-03 23:19:55 +0000

    PROY-2: Implementar analisis de ventas

--- scripts/analisis.py
+++ scripts/analisis.py
@@ -1,1 +1,20 @@
-print("Analisis ejecutado correctamente")
+import pandas as pd
+
+# Leer datos
+df = pd.read_csv("datos/ventas.csv")
+
+# Calcular importe
+df["importe"] = df["cantidad"] * df["precio"]
+
+# Ventas totales
+ventas_totales = df["importe"].sum()
+
+# Producto más vendido
+producto_mas_vendido = (
+    df.groupby("producto")["cantidad"]
+    .sum()
+    .idxmax()
+)
+
+print("VENTAS TOTALES:", ventas_totales)
+print("PRODUCTO MÁS VENDIDO:", producto_mas_vendido)
